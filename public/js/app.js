c43a1d8ac612f016c746cf0d1715c05e004ac9b1
ipatriots <nicolin16@gmail.com>
2015-09-13 14:28:28 -0400

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+
+$('nav');
+
+$('sidebar-right');
+
+$('.groups');
+$('.pages');
+
+$('#sidebar-right > div');
+
+$('.comments');
+
+$('.comments:first');
+
+$('.comments:last');
+
+
+$('.comments:nth-child(4)');
+
+$('.ads').hide();
+
+$('.status-update').show();
+
+$('.ads').show();
+
+$('nav').toggle()
+
+var $adverts = $('.ad-slot src').first();
+$adverts.attr('src', 'http://placebear.com/200/300')
+
+
+$('.post:last media-body p').text('willy wonka stole my secrete recipe')
+
+var $newsFeedLink = $('.post li:first-child');
+$newsFeedLink.addClass('post-liked');
+
+var $newsFeedLink = $('.post li:first-child');
+$newsFeedLink.removeClass('post-liked');
+
+var $newsFeedLink = $('.post li:first-child');
+$newsFeedLink.toggleClass('post-liked');
